0c4c374d9e7129d3b9a190360fdae5de5c0bdcc0
SankarReddy <SankarReddy@192.168.0.13>
2017-10-25 11:36:46 +0530

    1 st file change

---
+++ src/com/app/ConnectionUtil.java
@@ -0,0 +1,17 @@
+package com.app;
+
+import java.sql.Connection;
+
+
+public class ConnectionUtil 
+{
+	private static Connection con=null;
+	static
+	{
+		try {
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
